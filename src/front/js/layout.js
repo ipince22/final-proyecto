e33367c4c4aec8bc95675139939510a7280dd050
Daniel Ipince <ipince22@gmail.com>
2021-05-03 00:39:41 +0000

    trying to redeploy app

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
+//demo
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-
+//5/02/2021
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
